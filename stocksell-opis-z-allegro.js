b9af49c9307b32bc069e9c13ae024cbebde7e7c9
mikolajzieba-ui <mikolaj.zieba@estila.pl>
2026-06-23 13:05:58 +0200

    Create stocksell-opis-z-allegro.js
    Pobieranie opisu z allegro dla paczek zagranicznych + autofocus na pole skanowania po wydrukowaniu ponownie etykiety

---
+++ stocksell-opis-z-allegro.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Pobieranie opisu z Allegro dla StockSell
+// @namespace    http://tampermonkey.net/
+// @version      10.2
+// @description  Czysty Opis + Wady
+// @match        *://*.stocksell.io/*
+// @match        *://stocksell.io/*
+// @grant        GM_xmlhttpRequest
+// @connect      allegro.pl
+//
+// @downloadURL  https://raw.githubusercontent.com/TWOJ_LOGIN/stocksell-scripts/main/stocksell-opis-z-allegro.js
+// @updateURL    https://raw.githubusercontent.com/TWOJ_LOGIN/stocksell-scripts/main/stocksell-opis-z-allegro.js
+//
+// ==/UserScript==
